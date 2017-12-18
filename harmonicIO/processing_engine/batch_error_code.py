5ebe6eb0f2972217d0ccfee35ace273252f5ee70
Ben Blamey <blamey.ben@gmail.com>
2017-12-18 23:06:26 +0100

    make importable

---
+++ harmonicIO/processing_engine/batch_error_code.py
@@ -0,0 +1,5 @@
+class BatchErrorCode:
+    SUCCESS = 0
+    INVALID_PARAMETERS = 1
+    OPEN_SOCKET_ERROR = 2
+    DATA_SOCKET_ERROR = 3
--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup
 
 setup(name='harmonicIO_PE',
       packages=['harmonicIO.processing_engine'],
+      install_requires=['requests', 'urllib3']
       )
