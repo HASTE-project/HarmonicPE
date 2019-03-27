1f8440f2eb56ec5cb779a44f5e0165c9f65bae31
Oliver Stein <oliver.stein.privat@gmail.com>
2019-03-27 19:31:19 +0100

    changed method of busying CPU

--- example.py
+++ example.py
@@ -4,10 +4,8 @@ import time
 import subprocess
 from os import environ
 
-def busyfunction(busytime, cpulevel):
-    p = subprocess.Popen(['lookbusy',
-                          '--ncpus', environ.get("NUM_CORES", "1"),
-                          '--cpu-util', cpulevel])
+def busyfunction(busytime):
+    p = subprocess.Popen(['python', './dummyload.py'])
     time.sleep(busytime)
     p.terminate()
     p.communicate()
@@ -15,11 +13,10 @@ def busyfunction(busytime, cpulevel):
 
 def process_data(data):
     # Format of binary message representing task for distributed execution is specific to your application.
-    print('Busying the CPU...', flush=True)
+    print('Busying the CPU...\n', flush=True)
     busytime = int(environ.get("BUSY_TIME", 60))
-    cpulevel = environ.get("CPU_LEVEL", "20")
     # Busy CPU some time
-    busyfunction(busytime, cpulevel)
+    busyfunction(busytime)
 
 
 
