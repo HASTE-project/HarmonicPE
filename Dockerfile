1e694acd40ae12659c30f2181ea193f466dbfb70
Oliver Stein <oliver.stein.privat@gmail.com>
2019-12-11 22:47:12 +0100

    started cellprofiler processing engine

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
+
 # HIO requires python 3.
-FROM python:3.6.3
+FROM dahlo/cellprofiler:3.1.9-16.04
+
+RUN apt update && apt install -y python3.5 python3-pip
+RUN pip3 install Pillow
+
 
 # Set the working directory to /app
 WORKDIR /app
 
 # Checkout and install Harmonic PE:
-RUN git clone https://github.com/HASTE-project/HarmonicPE.git && cd /app/HarmonicPE && git checkout thesiswork_testing && pip install .
+RUN git clone https://github.com/HASTE-project/HarmonicPE.git && cd /app/HarmonicPE && git checkout cellprofiler-PE && pip3 install -e .
 
 # Make port 80 available (required for the listening daemon)
 EXPOSE 80
@@ -17,7 +22,7 @@ ADD example.py /app
 
 # Add arguments for lookbusy
 ARG cpulevel=20
-ARG busytime=60
+ARG busytime=10
 ADD dummyload.py /app/dummyload.py
 
 ENV CPU_LEVEL=${cpulevel}
@@ -25,5 +30,5 @@ ENV BUSY_TIME=${busytime}
 
 # Run your script on startup, to start the daemon:
 WORKDIR /app
-
-CMD ["python", "example.py"]
+ENTRYPOINT []
+CMD ["python3.5", "example.py"]
--- example.py
+++ example.py
@@ -1,8 +1,12 @@
 from harmonicPE.daemon import listen_for_tasks
-
+import json
 import time
 import subprocess
 from os import environ
+import uuid
+from PIL import Image
+import os, io
+
 
 def busyfunction(busytime):
     p = subprocess.Popen(['python', './dummyload.py'])
@@ -11,14 +15,55 @@ def busyfunction(busytime):
     p.communicate()
     del p
 
-def process_data(data):
+def old_process_data(data):
     # Format of binary message representing task for distributed execution is specific to your application.
     print('Busying the CPU...\n', flush=True)
     busytime = int(environ.get("BUSY_TIME", 60))
     # Busy CPU some time
     busyfunction(busytime)
 
+def get_metadata_and_data(message_bytes):
 
+    sep_point = message_bytes.find(b';')
+    b_metadata = message_bytes[0:sep_point-1]
+    b_data = message_bytes[sep_point+2:]
+    return b_metadata, b_data
+
+def process_data(message_bytes):
+    # Format of binary message representing task for distributed execution is specific to your application.
+    print('message was bytes: ' + str(len(message_bytes)), flush=True)
+    b_metadata, b_data = get_metadata_and_data(message_bytes)
+
+    image_file =  b_data
+
+    metadata_str = b_metadata.decode('utf-8')
+    metadata = json.loads(metadata_str)
+    not_unique_id = 1
+    uuid_local_dir = None
+    local_file = None
+    path_to_data_file = None
+    path_to_dir = None
+    while not_unique_id:
+        uuid_local_dir = str(uuid.uuid1())
+        local_file = metadata['name']
+        cwd = os.getcwd()
+        path_to_dir = cwd+'/'+uuid_local_dir
+        path_to_data_file = cwd+'/'+uuid_local_dir+'/'+local_file
+        if not os.path.exists(path_to_dir):
+            os.makedirs(path_to_dir)
+            if not os.path.exists(path_to_data_file):
+                image = Image.open(io.BytesIO(image_file))
+                image.save(path_to_data_file)
+                not_unique_id = 0
+                break
+        else:
+            continue
+    status, output = subprocess.getstatusoutput('cellprofiler -p Salman_CellProfiler_cell_counter_no_specified_folders.cpproj -i '+ path_to_dir + ' &> /dev/null')
+    if status == 0:
+        subprocess.getstatusoutput('rm -r '+path_to_dir)
+        print("image analysis complete!")
+    else:
+        pass
 
 # Start the daemon to listen for tasks:
 listen_for_tasks(process_data)
